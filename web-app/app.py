cf1ab4e8af4efbd815a8763148851e8e954b2c29
Zeeshan <zeeshan.ijaz@emumba.com>
2023-01-24 17:59:42 +0500

    connect app to db

--- web-app/app.py
+++ web-app/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 db = SQLAlchemy(app)
 class Item(db.Model):
-  id = db.Column(db.Integer, primary_key=True)
+  id = db.Column(db.Integer, primary_key=True, autoincrement=True)
   title = db.Column(db.String(80), unique=True, nullable=False)
   content = db.Column(db.String(120), unique=True, nullable=False)
 
@@ -17,7 +17,7 @@ db.create_all()
 def get_item(id):
   item = Item.query.get(id)
   del item.__dict__['_sa_instance_state']
-  return jsonify(item)
+  return jsonify(item.__dict__)
 
 
 @app.route('/items', methods=['POST'])
